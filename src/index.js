076bb8008b90a8673d4305391281fcbeeecb0ce3
Safenorsound <ijroback@gmail.com>
2020-06-18 14:26:04 -0700

    update JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchMovies from './searchMovies';
+import styles from './index.css';
 
 class Main extends React.Component {
   render() {
--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+const searchMovies = async (e) => {
+  e.preventDefault();
+  console.log('submitting');
+
+  const query = 'Jurassic Park';
+
+  const url =
+    'https://api.themoviedb.org/3/movie/550?api_key=c422e2bbed9ed9d724c2a7899bdf350d&language=en-US&query=${query}&page=1&include_adult=true';
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export default function SearchMovies() {
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
@@ -10,7 +28,7 @@ export default function SearchMovies() {
         className="input"
         type="text"
         name="query"
-        placeholder="Enter movie search here"
+        placeholder="Enter movie"
       />
       <button className="button" type="submit">
         Search
